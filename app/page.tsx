00517978c5cfbe3ed3c4219ba02d7da308397145
OssianN <ossian.northen@gmail.com>
2023-10-09 14:01:05 +0200

    adds home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,53 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+import { useEffect, useState } from 'react'
+import { Board } from '@/components/GameBoard/Board'
+import KeybaordContainer from '@/components/Keyboard/KeyboardContainer'
+import { Result } from '@/types'
+import { addKey, checkResult } from '@/utils/BoardUtils'
+import styles from './home.module.css'
 
 export default function Home() {
+  const [rows, setRows] = useState<string[][]>([[]])
+  const [result, setResult] = useState<Result[][]>([])
+  const currentIndex = rows.length - 1
+
+  useEffect(() => {
+    const handleKeyDown = async (e: KeyboardEvent) => {
+      const isKeyOrCommand = /^(?:[A-Za-z]|Enter|Backspace)$/
+
+      if (!isKeyOrCommand.test(e.key)) return
+      if (e.key === 'Enter' && rows[currentIndex].length !== 4) return
+
+      if (e.key === 'Enter') {
+        const newResultRow = await checkResult(rows, currentIndex)
+        setResult([...result, newResultRow])
+        setRows([...rows, []])
+        return
+      }
+
+      if (e.key === 'Backspace') {
+        rows[currentIndex].pop()
+        setRows([...rows])
+        return
+      }
+
+      if (rows[currentIndex].length < 4) {
+        const newRow = addKey(rows, currentIndex, e)
+        setRows(newRow)
+        return
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyDown)
+
+    return () => window.removeEventListener('keydown', handleKeyDown)
+  }, [currentIndex, result, rows])
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <h1 className={styles.title}>Fourdle</h1>
+      <Board rows={rows} result={result} />
+      <KeybaordContainer result={result.flat()} />
     </main>
   )
 }
