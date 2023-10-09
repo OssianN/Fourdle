0003dfc9174c585f254ab94743fa800d31f72552
OssianN <ossian.northen@gmail.com>
2023-10-09 14:01:15 +0200

    adds api route for checking result

---
+++ app/api/check/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server'
+
+export async function POST(req: Request) {
+  const body = await req.json()
+  const response = await fetch('https://fourdle-api.vercel.app/validate/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ word: body.word }),
+  })
+
+  const data: string[] = await response.json()
+  const result = data.map((color, i) => ({ color, letter: body.word[i] }))
+  console.log(result)
+
+  return NextResponse.json(result)
+}
