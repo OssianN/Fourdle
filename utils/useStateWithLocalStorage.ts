bfb7c562701999c6219c3f47402cce0899b669a5
OssianN <ossian.northen@gmail.com>
2023-10-26 21:41:04 +0200

    adds reset of localstorage

--- utils/useStateWithLocalStorage.ts
+++ utils/useStateWithLocalStorage.ts
@@ -10,21 +10,25 @@ export const useStateWithLocalStorage = <T>({
 }) => {
   const [state, setState] = useState<T>(initialState)
   const [firstLoad, setFirstLoad] = useState<boolean>(false)
+  const today = new Date().getDay()
 
   useEffect(() => {
     setFirstLoad(true)
 
     const stored = localStorage.getItem(key)
     if (!stored) return
+    if (JSON.parse(stored).expiry < today) {
+      return
+    }
 
-    setState(JSON.parse(stored))
-  }, [key])
+    setState(JSON.parse(stored).state)
+  }, [key, today])
 
   useEffect(() => {
     if (!firstLoad) return
 
-    localStorage.setItem(key, JSON.stringify(state))
-  }, [firstLoad, key, state])
+    localStorage.setItem(key, JSON.stringify({ state, expiry: today }))
+  }, [firstLoad, key, state, today])
 
   return { state, setState }
 }
